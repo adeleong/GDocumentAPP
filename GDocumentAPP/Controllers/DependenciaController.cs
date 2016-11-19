d2a8078e85a0a7132b03aa27379c5ed40d3e03e1
tirsoortiz <tirsorortiz@gmail.com>
2016-11-19 15:01:24 -0400

    Creacion de dependencia

---
+++ GDocumentAPP/Controllers/DependenciaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using GDocumentAPP.Context;
+
+namespace GDocumentAPP.Controllers
+{
+    public class DependenciaController : Controller
+    {
+        private GDocumentDBEntities db = new GDocumentDBEntities();
+
+        // GET: Dependencia
+        public ActionResult Index()
+        {
+            return View(db.DEPENDENCIAs.ToList());
+        }
+
+        // GET: Dependencia/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DEPENDENCIA dEPENDENCIA = db.DEPENDENCIAs.Find(id);
+            if (dEPENDENCIA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dEPENDENCIA);
+        }
+
+        // GET: Dependencia/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Dependencia/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DEPENDENCIA_ID,DEPENDENCIA_NOMBRE")] DEPENDENCIA dEPENDENCIA)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DEPENDENCIAs.Add(dEPENDENCIA);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(dEPENDENCIA);
+        }
+
+        // GET: Dependencia/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DEPENDENCIA dEPENDENCIA = db.DEPENDENCIAs.Find(id);
+            if (dEPENDENCIA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dEPENDENCIA);
+        }
+
+        // POST: Dependencia/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DEPENDENCIA_ID,DEPENDENCIA_NOMBRE")] DEPENDENCIA dEPENDENCIA)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dEPENDENCIA).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(dEPENDENCIA);
+        }
+
+        // GET: Dependencia/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DEPENDENCIA dEPENDENCIA = db.DEPENDENCIAs.Find(id);
+            if (dEPENDENCIA == null)
+            {
+                return HttpNotFound();
+            }
+            return View(dEPENDENCIA);
+        }
+
+        // POST: Dependencia/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            DEPENDENCIA dEPENDENCIA = db.DEPENDENCIAs.Find(id);
+            db.DEPENDENCIAs.Remove(dEPENDENCIA);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
