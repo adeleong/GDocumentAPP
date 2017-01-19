de26c1feb9208c94fd74a05d1abe39208aa95625
adeleong <aderso.deleon@gmail.com>
2017-01-19 17:53:35 -0400

    Configurando count dashboard

---
+++ GDocumentAPP/Models/DashboardViewModels.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GDocumentAPP.Models
+{
+    public class DashboardViewModels
+    {
+
+        private ModelDocumentoApp db = new ModelDocumentoApp();
+
+        public int CountPersona()
+        {
+            return  db.PERSONAs.Count();
+        }
+
+        public int CountEmpleadoActivos()
+        {
+
+            return db.EMPLEADOes.Where( e => e.ESTATU.DESCRIPCION.ToLower().Equals("activo") ).Count(); ;
+        }
+
+        public int CountExpedienteDigitales()
+        {
+            return 0;
+        }
+
+        public int CountExpedienteFisico()
+        {
+            return 0;
+        }
+
+    }
+}
